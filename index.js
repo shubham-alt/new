9c4f8b278602ddf19913c22591f7a414c0419072
shubham-alt <58552516+shubham-alt@users.noreply.github.com>
2019-12-12 15:29:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const db = require('./queries')
-const port = 5432
+const port = 4000
 
 app.use(bodyParser.json())
 app.use(
